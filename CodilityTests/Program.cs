2b6325279e1cfe875e57bbd7bb0685c9aed516ed
Munawar <needo@msn.com>
2022-07-31 23:04:16 +0500

    Added BinaryGap

--- CodilityTests/Program.cs
+++ CodilityTests/Program.cs
@@ -10,18 +10,23 @@ namespace CodilityTests
             Console.Read();
         }
         private static void Run()
-        { 
+        {
+            //Iterations
+            int input = 1041;
+            var res1 =BinaryGap.solution(input);
+            Console.WriteLine($"BinaryGap for {input} is: {res1}");            
+
             //Arrays
             int[] B = { 9, 3, 9, 3, 9, 7, 9 };
             var resOdd = OddOccuranceInArray.solution(B);
             var resB = OddOccuranceInArray.solutionB(B);
             Console.WriteLine($"Odd number missing pair : {string.Join(",", resOdd)} | solution B: {string.Join(",", resB)}");
 
-            //Iterations
+            //Arrays
             int[] A = { 3, 8, 9, 7, 6 };
             int K = 3;
-            var res = CyclicRotate.solution(A, K);
-            Console.WriteLine($"After {K} rotatations : {string.Join(",", res)}");
+            var res2 = CyclicRotate.solution(A, K);
+            Console.WriteLine($"After {K} rotatations : {string.Join(",", res2)}");
 
         }
     }
---
+++ CodilityTests/Solutions/BinaryGap.cs
@@ -0,0 +1,47 @@
+﻿namespace CodilityTests.Solutions
+{
+    internal static class BinaryGap
+    {
+        public static int solution(int input)
+        {
+            int inputNumber = input;
+            int result, gap, lastGap, numberOfGaps, remainder;
+            result = gap = lastGap = numberOfGaps = remainder = 0;
+            bool isFirstOnDigit = false;
+
+            string binaryResult = "";
+            while (inputNumber > 0)
+            {
+                remainder = inputNumber % 2;
+                inputNumber = inputNumber / 2;
+
+                binaryResult = remainder.ToString() + binaryResult;
+
+                if (remainder == 1)
+                {
+                    if (!isFirstOnDigit)
+                    {
+                        isFirstOnDigit = true;
+                    }
+                    else
+                    {
+                        if (gap > 0)
+                            numberOfGaps += 1;
+
+                        if (gap > lastGap)
+                            lastGap = gap;
+                    }
+
+                    gap = 0;
+                }
+                else
+                    gap += 1;
+            }
+
+            if (numberOfGaps >= 1 && lastGap > 0)
+                result = lastGap;
+           
+            return result;
+        }
+    }
+}
