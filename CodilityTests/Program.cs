bcf1e8c8ee1a7f22e1c24cc621f4e0e28262463d
Munawar <needo@msn.com>
2022-07-31 22:43:51 +0500

    OddOccurrencesInArray Find value that occurs in odd number of elements.

--- CodilityTests/Program.cs
+++ CodilityTests/Program.cs
@@ -5,27 +5,24 @@ namespace CodilityTests
     {
         static void Main(string[] args)
         {
+
+            Run();
+            Console.Read();
+        }
+        private static void Run()
+        { 
+            //Arrays
+            int[] B = { 9, 3, 9, 3, 9, 7, 9 };
+            var resOdd = OddOccuranceInArray.solution(B);
+            var resB = OddOccuranceInArray.solutionB(B);
+            Console.WriteLine($"Odd number missing pair : {string.Join(",", resOdd)} | solution B: {string.Join(",", resB)}");
+
+            //Iterations
             int[] A = { 3, 8, 9, 7, 6 };
             int K = 3;
-            var res=CyclicRotate.solution(A, K);
-            Console.WriteLine($"After {K} rotatations : { string.Join(",",res) }");
+            var res = CyclicRotate.solution(A, K);
+            Console.WriteLine($"After {K} rotatations : {string.Join(",", res)}");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            Console.Read();
         }
     }
 }
---
+++ CodilityTests/Solutions/OddOccuranceInArray.cs
@@ -0,0 +1,34 @@
+﻿namespace CodilityTests.Solutions
+{
+    internal static class OddOccuranceInArray
+    {
+        //A[0] = 9  A[1] = 3  A[2] = 9
+        //A[3] = 3  A[4] = 9  A[5] = 7
+        //A[6] = 9
+        public static int solution(int[] A)
+        {
+            HashSet<int> set = new HashSet<int>();
+            foreach (int num in A)
+            {
+                //Removing means, it was added on first occurance and on second occurance is removed. that means, anything left at the end does not have pairs.
+                if (set.Contains(num))
+                    set.Remove(num);
+                else
+                    set.Add(num);
+            }
+
+            int result = (int)set.LastOrDefault(1);
+            return result;
+        }
+
+        public static int solutionB(int[] A)
+        {
+            int monMatched = A[0];
+            for (int i = 1; i < A.Length; i++)
+            {
+                monMatched = monMatched ^ A[i];//if corresponding bits are same means numbers are equal then it returns 0 otherwise
+            }
+            return monMatched;
+        }
+    }
+}
